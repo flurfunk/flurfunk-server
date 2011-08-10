30bfd937669304e72b80d2577a763913880ac852
Felix H. Dahlke <fhd@ubercode.de>
2011-08-10 14:35:06 +0200

    REST API: List messages

--- src/main/clojure/flurfunk/core.clj
+++ src/main/clojure/flurfunk/core.clj
@@ -1,17 +1,36 @@
 (ns flurfunk.core
   "The core service of Flurfunk, provides the index page and the REST API."
   (:use compojure.core
-    ring.util.servlet
-    hiccup.core)
+        ring.util.servlet
+        hiccup.core)
   (:require [compojure.route :as route])
   (:gen-class
-    :extends javax.servlet.http.HttpServlet))
+   :extends javax.servlet.http.HttpServlet))
+
+(def messages [])
+
+(defn get-messages []
+  messages)
+
+(defn marshal-messages [messages]
+  (let [messages (get-messages)]
+    (if (not (empty? messages))
+      (reduce (fn [x y] (str x y))
+              (map (fn [message]
+                     (str "<message id='" (:id message)
+                          "' author='" (:author message)
+                          "'>" (:body message) "</message>"))
+                   messages)))))
 
 (defroutes main-routes
   (GET "/" [] (html
-    [:head [:title "Flurfunk"]]
-    [:body
-     [:script {:src "flurfunk.js"}]]))
+               [:head [:title "Flurfunk"]]
+               [:body
+                [:script {:src "flurfunk.js"}]]))
+  (GET "/messages" []
+       (str "<messages>"
+            (marshal-messages (get-messages))
+            "</messages>"))
   (route/files "/" {:root "src/main/webapp"})
   (route/not-found "Page not found"))
 
--- src/test/clojure/flurfunk/test_core.clj
+++ src/test/clojure/flurfunk/test_core.clj
@@ -1,11 +1,48 @@
 (ns flurfunk.test-core
   (:use clojure.test
-    clojure.contrib.string
-    flurfunk.core))
+        clojure.contrib.string
+        flurfunk.core)
+  (:require [clojure.xml :as xml]
+            [clojure.contrib.duck-streams :as streams]
+            [clojure.contrib.io :as io]))
 
-(defn request [resource routes & params]
-  (routes {:request-method :get :uri resource :params (first params)}))
+(defn request [resource & params]
+  (main-routes {:request-method :get :uri resource :params (first params)}))
+
+(defn request-body [resource]
+  (:body (request resource)))
+
+(defn parsed-request-body [resource]
+  (xml/parse (io/input-stream (streams/to-byte-array
+                               (request-body resource)))))
+
+(defn parse-messages [xml]
+  (map (fn [x]
+         (let [attrs (:attrs x)]
+           {:body (first (:content x))
+            :id (:id attrs)
+            :author (:author attrs)}))
+       (:content xml)))
 
 (deftest test-index-page
   (is (substring? "flurfunk.js"
-    (:body (request "/" main-routes)))))
+                  (request-body "/"))))
+
+(deftest test-get-messages-successful
+  (is (= 200 (:status (request "/messages")))))
+
+(deftest test-get-messages-empty
+  (is (empty? (parse-messages (parsed-request-body "/messages")))))
+
+(deftest test-get-messages-body
+  (binding [get-messages (fn [] [{:body "foo" :id "1337"}])]
+    (is (= "foo"
+           (:body (first (parse-messages
+                          (parsed-request-body "/messages"))))))))
+
+(deftest test-get-messages-attributes
+  (binding [get-messages (fn [] [{:body "foo" :id "1337" :author "thomas"}])]
+    (let [messages (parse-messages (parsed-request-body "/messages"))
+          message (first messages)]
+      (are (= "1337" (:id message))
+           (= "thomas" (:author message))))))
