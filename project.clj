46e478497a4f794a02ce5c5c859d4d3115748aee
Felix H. Dahlke <fhd@ubercode.de>
2011-09-19 12:35:00 +0200

    Set project name and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject foo "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject flurfunk "0.1.0-SNAPSHOT"
+  :description "The Funk of the Flur"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure/compojure "0.6.5"]]
