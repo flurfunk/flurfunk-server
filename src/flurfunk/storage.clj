a2427721f372533deee21f6bd2165a18855a5559
Felix H. Dahlke <fhd@ubercode.de>
2011-09-20 18:28:31 +0200

    Use FleetDB for persistent storage

--- src/flurfunk/storage.clj
+++ src/flurfunk/storage.clj
@@ -1,17 +1,58 @@
 (ns flurfunk.storage
-  "Storage and retrieval of objects.")
+  "Storage and retrieval of objects."
+  (:require [fleetdb.client :as db]
+	    [clojure.walk :as walk]))
 
-(def messages [])
+(defprotocol Storage
+  (storage-get-messages [this])
+  (storage-add-message [this message])
+  (storage-find-message [this id]))
+
+(deftype MemoryStorage [messages] Storage
+  (storage-get-messages [this] @messages)
+
+  (storage-add-message
+   [this message]
+   (let [message-with-id (conj message {:id (str (count @messages))})]
+     (swap! messages (fn [messages]
+                       (cons message-with-id messages)))))
+
+  (storage-find-message
+   [this id]
+   (first (filter (fn [message] (= id (:id message))) @messages))))
+
+(deftype FleetDBStorage [client] Storage
+  (storage-get-messages
+   [this]
+   (walk/keywordize-keys (client ["select" "messages"
+                                  {"order" ["id", "desc"]}])))
+
+  (storage-add-message
+   [this message]
+   ;; TODO: Find a better way to generate the ID.
+   (let [message-with-id (conj message {:id (str (System/currentTimeMillis))})]
+     (client ["insert" "messages" message-with-id])))
+
+  (storage-find-message
+   [this id]
+   (walk/keywordize-keys (first (client ["select" "messages"
+                                         {"where" ["=" :id id]}])))))
+
+(defn- make-storage []
+  (if (= (System/getProperty "fleetdb") "true")
+    (FleetDBStorage. (db/connect {:host "127.0.0.1" :port 3400}))
+    (do
+      (println (str "Using memory database. Set the system property "
+                    "\"flurfunk.fleetdb\" to \"true\" if you want a real one."))
+      (MemoryStorage. (atom [])))))
+
+(def ^{:private true} storage (make-storage))
 
 (defn get-messages []
-  messages)
-
-(defn- generate-id []
-  (str (System/currentTimeMillis)))
+  (storage-get-messages storage))
 
 (defn add-message [message]
-  (let [message-with-id (conj message {:id (generate-id)})]
-  (def messages (cons message-with-id messages))))
+  (storage-add-message storage message))
 
 (defn find-message [id]
-  (first (filter (fn [message] (= id (:id message))) messages)))
+  (storage-find-message storage id))
--- test/flurfunk/test_storage.clj
+++ test/flurfunk/test_storage.clj
@@ -6,10 +6,10 @@
   (add-message {:author "felix" :body "foobar"})
   (let [first-message (first (get-messages))]
     (are [v k] (= v (k first-message))
-         "foobar" :body
-         "felix" :author)
+	 "foobar" :body
+	 "felix" :author)
     (is (not (empty? (:id first-message)))
-        "An ID should be generated for the new message.")))
+	"An ID should be generated for the new message.")))
 
 (deftest test-find-message
   (add-message {:author "felix" :body "foobar"})
@@ -17,6 +17,11 @@
     (add-message {:author "thomas" :body "barfoo"})
     (let [message (find-message id)]
       (are [v k] (= v (k message))
-           "barfoo" :body
-           "thomas" :author))))
-    
+	   "foobar" :body
+	   "felix" :author))))
+
+(deftest test-ordered-messages
+  (add-message {:author "felix" :body "first"})
+  (is (= "felix" (:author (first (get-messages)))))
+  (add-message {:author "thomas" :body "second"})
+  (is (= "thomas" (:author (first (get-messages))))))
