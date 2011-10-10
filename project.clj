46293ac5690095fb69f9151072d8a750ac8d6599
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-10-10 12:33:56 +0200

    Configure viaboxx maven repos, and omit the lein defauls

--- project.clj
+++ project.clj
@@ -5,4 +5,10 @@
                  [compojure/compojure "0.6.5"]
                  [fleetdb-client "0.2.2"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
-  :ring {:handler flurfunk.routes/app})
+  :ring {:handler flurfunk.routes/app}
+  :omit-default-repositories true
+  :repositories {
+      "viaboxx-nexus" {:url "https://www.viaboxxsystems.de/nexus/content/groups/public/"}
+      "viaboxx-releases" {:url "https://www.viaboxxsystems.de/nexus/content/repositories/releases/"}
+      "viaboxx-snapshots" {:url "https://www.viaboxxsystems.de/nexus/content/repositories/snapshots/"}}
+)
